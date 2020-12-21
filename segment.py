8699eaf20297f0255807d6b995b09c6208a29c2a
Youssef Walid <yousefwalid98@hotmail.com>
2020-12-21 22:12:56 +0200

    Fixed a bug where divide_segment used to wrap around and segment incorrectly

--- segment.py
+++ segment.py
@@ -11,23 +11,16 @@ connectedNotes = connect_notes(removedLines, staffDim)
 boundingBoxes = get_bounding_boxes(connectedNotes)
 
 show_images(
-  [image, linesOnly, removedLines, connectedNotes],
-  ['Original', 'Lines Only', 'Image - Lines', 'Closing(Image - Lines)']
+    [image, linesOnly, removedLines, connectedNotes],
+    ['Original', 'Lines Only', 'Image - Lines', 'Closing(Image - Lines)']
 )
 
 dirtyMasked = mask_image(connectedNotes, image)
 masked = remove_non_vertical_protrusions(dirtyMasked, staffDim)
-# heads = isolate_heads(masked, staffDim)
-# vHist = get_vertical_histogram(heads)
-# vHistImg = get_vertical_histogram_image(vHist, heads.shape)
-# show_images(
-#   [image, masked, heads, vHistImg],
-#   ['Original Image', 'Masking', 'Isolated heads', 'Vertical histogram']
-# )
 
 show_images(
-  [image, masked],
-  ['Original Image', 'Masking']
+    [image, masked],
+    ['Original Image', 'Masking']
 )
 
 # Get slices of masked
@@ -35,5 +28,6 @@ slicedBoxesofImage = slice_image(image, boundingBoxes)
 slicedBoxesOfMasked = slice_image(masked, boundingBoxes)
 
 # Segment everything
-allSegments = segment_image(slicedBoxesofImage, slicedBoxesOfMasked, boundingBoxes)
+allSegments = segment_image(
+    slicedBoxesofImage, slicedBoxesOfMasked, boundingBoxes)
 show_images(allSegments)
