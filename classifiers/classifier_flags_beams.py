b3c6c65315ac6b026985447a690d45ee3916f388
Ahmed Hisham <ahisham360@gmail.com>
2021-01-04 17:18:31 +0200

    Added flag_beam classifier but needs more dataset for flags

---
+++ classifiers/classifier_flags_beams.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from featureExtractor import *
+
+path_to_dataset = r'dataset_flags_beams'
+
+
+run_experiment('hog','classifier_flag_beam',path_to_dataset)
