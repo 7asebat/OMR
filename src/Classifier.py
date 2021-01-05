a3c4b03afc475bff733e9072a588f072e46ac3d7
Abdelrahman Farid <abdelrahman.farid99@eng-st.cu.edu.eg>
2021-01-05 12:00:57 +0200

    Small refactoring changes

--- src/Classifier.py
+++ src/Classifier.py
@@ -11,19 +11,14 @@ from Component import *
 class Classifier:
     __classifiers = {}
 
-    def load_classifiers():
-        pathList = ['classifier_notes_accidentals',
-                    'classifier_accidentals',
-                    'classifier_holes',
-                    'classifier_beams']
-
-        for path in pathList:
-            Classifier.__classifiers[path] = Classifier(f'classifiers/{path}')
+    def load_classifiers(classifiers):
+        for c, cd in classifiers.items():
+            Classifier.__classifiers[c] = Classifier(cd['path'], cd['featureSet'])
 
     def assign_note_accidental(image, baseComponents):
         for i, cmp in enumerate(baseComponents):
             slc = image[cmp.slice]
-            clf = Classifier.__classifiers['classifier_notes_accidentals']
+            clf = Classifier.__classifiers['note_accidental']
             tp = clf.extract_and_predict(slc)[0]
 
             if tp == 'Accidental':
@@ -31,21 +26,22 @@ class Classifier:
                 Classifier.assign_accidental_kind(image, baseComponents[i])
 
             elif tp == 'Note':
-                if (type(baseComponents[i]) != Note):
+                # Beamed note
+                if type(baseComponents[i]) is Note:
+                    Classifier.assign_beamed_note_timing(image, baseComponents[i])
+
+                else:
                     baseComponents[i] = Note(cmp.box)
                     Classifier.assign_note_filled(image, baseComponents[i])
 
-                else:
-                    Classifier.assign_beamed_note_timing(image, baseComponents[i])
-
     def assign_accidental_kind(image, accidental):
         slc = image[accidental.slice]
-        clf = Classifier.__classifiers['classifier_accidentals']
+        clf = Classifier.__classifiers['accidental_kind']
         accidental.kind = clf.extract_and_predict(slc)[0]
 
     def assign_note_filled(image, note):
         slc = image[note.slice]
-        clf = Classifier.__classifiers['classifier_holes']
+        clf = Classifier.__classifiers['note_filled']
         note.filled = clf.extract_and_predict(slc)[0] == 'filled'
 
         if note.filled:
@@ -59,15 +55,20 @@ class Classifier:
 
     def assign_beamed_note_timing(image, note):
         slc = image[note.slice]
-        clf = Classifier.__classifiers['classifier_beams']
+        clf = Classifier.__classifiers['beamed_note_timing']
         note.timing = clf.extract_and_predict(slc, 'weighted_line_peaks')[0]
 
-    def __init__(self, path):
+    def __init__(self, path=None, featureSet=None):
         self.model = None
+        self.featureSet = featureSet
         if path:
             self.model = pickle.load(open(path, 'rb'))
 
-    def extract_and_predict(self, image, featureSet='hog'):
+    def extract_and_predict(self, image, featureSet=None):
+        # Use the classifier's configured feature set
+        if not featureSet:
+            featureSet = self.featureSet
+        
         extractedFeatures = FeatureExtractor.extract(image, featureSet)
         return self.model.predict([extractedFeatures])
 
