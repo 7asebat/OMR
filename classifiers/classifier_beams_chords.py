6c47f4df7cf334d494e2ca82f323c874a4cfbb35
Ahmed Hisham <ahisham360@gmail.com>
2021-01-06 20:05:26 +0200

    Added chords dataset,and classifier chords_beams

---
+++ classifiers/classifier_beams_chords.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from featureExtractor import *
+
+path_to_dataset = r'dataset_beams_chords'
+
+
+run_experiment('hog','classifer_beams_chords',path_to_dataset)
