8c2640b9aee54b76ec0d0a5b00f9754b5f871800
Youssef Walid <yousefwalid98@hotmail.com>
2021-01-09 23:14:38 +0200

    Working on some captured cases

--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -11,7 +11,7 @@ from Display import show_images
 
 
 def should_rotate_edge_image(edge_image):
-    h, theta, d = hough_line(edge_image)
+    h, theta, _ = hough_line(edge_image)
     max_index = np.unravel_index(h.argmax(), h.shape)
     degreePeak = np.rad2deg(theta[max_index[1]])
     if(abs(degreePeak) > 88 and abs(degreePeak) < 92):
@@ -20,9 +20,6 @@ def should_rotate_edge_image(edge_image):
 
 
 def biggest_contour(contours, min_area):
-    # ## **Find the Biggest Contour**
-    # **Note: We made sure the minimum contour is bigger than 1/10 size of the whole picture. This helps in removing very small contours (noise) from our dataset**
-    biggest = None
     max_area = 0
     biggest_n = 0
     approx_contour = None
@@ -42,82 +39,46 @@ def biggest_contour(contours, min_area):
 
 
 def order_points(pts):
-    # initialzie a list of coordinates that will be ordered
-    # such that the first entry in the list is the top-left,
-    # the second entry is the top-right, the third is the
-    # bottom-right, and the fourth is the bottom-left
     pts = pts.reshape(4, 2)
     rect = np.zeros((4, 2), dtype="float32")
 
-    # the top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
     s = pts.sum(axis=1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
 
-    # now, compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
     diff = np.diff(pts, axis=1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
 
-    # return the ordered coordinates
     return rect
 
 
-# ## Find the exact (x,y) coordinates of the biggest contour and crop it out
-
-
 def four_point_transform(image, pts):
-    # obtain a consistent order of the points and unpack them
-    # individually
+
     rect = order_points(pts)
     (tl, tr, br, bl) = rect
 
-    # compute the width of the new image, which will be the
-    # maximum distance between bottom-right and bottom-left
-    # x-coordiates or the top-right and top-left x-coordinates
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
     maxWidth = max(int(widthA), int(widthB))
 
-    # compute the height of the new image, which will be the
-    # maximum distance between the top-right and bottom-right
-    # y-coordinates or the top-left and bottom-left y-coordinates
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
 
-    # now that we have the dimensions of the new image, construct
-    # the set of destination points to obtain a "birds eye view",
-    # (i.e. top-down view) of the image, again specifying points
-    # in the top-left, top-right, bottom-right, and bottom-left
-    # order
     dst = np.array([
         [0, 0],
         [maxWidth - 1, 0],
         [maxWidth - 1, maxHeight - 1],
         [0, maxHeight - 1]], dtype="float32")
 
-    # compute the perspective transform matrix and then apply it
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 
-    # return the warped image
     return warped
 
 
 def simplify_contour(contour, n_corners=4):
-    '''
-    Binary searches best `epsilon` value to force contour 
-        approximation contain exactly `n_corners` points.
-
-    :param contour: OpenCV2 contour.
-    :param n_corners: Number of corners (points) the contour must contain.
-
-    :returns: Simplified contour in successful case. Otherwise returns initial contour.
-    '''
     n_iter, max_iter = 0, 100
     lb, ub = 0., 1.
 
@@ -138,74 +99,6 @@ def simplify_contour(contour, n_corners=4):
             return approx
 
 
-def simplify_contour_2(c):
-    pass
-
-
-def show_image(img):
-    cv2.imshow("Image", img)
-    # press q or Esc to close
-    cv2.waitKey(0)
-
-
-def fix_skewing(im, max_skew=10):
-    im_gs = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    im_gs = cv2.fastNlMeansDenoising(im_gs, h=3)
-    height, width = im_gs.shape
-
-    # im = (im * 255).astype(np.uint8)
-
-    # Detect lines in this image. Parameters here mostly arrived at by trial and error.
-    lines = cv2.HoughLinesP(
-        im_gs, 1, np.pi / 180, 200, minLineLength=width / 12, maxLineGap=width / 150
-    )
-
-    # Collect the angles of these lines (in radians)
-    angles = []
-    for line in lines:
-        x1, y1, x2, y2 = line[0]
-        angles.append(np.arctan2(y2 - y1, x2 - x1))
-
-    # If the majority of our lines are vertical, this is probably a landscape image
-    landscape = np.sum(
-        [abs(angle) > np.pi / 4 for angle in angles]) > len(angles) / 2
-
-    # Filter the angles to remove outliers based on max_skew
-    if landscape:
-        angles = [
-            angle
-            for angle in angles
-            if np.deg2rad(90 - max_skew) < abs(angle) < np.deg2rad(90 + max_skew)
-        ]
-    else:
-        angles = [angle for angle in angles if abs(
-            angle) < np.deg2rad(max_skew)]
-
-    if len(angles) < 5:
-        # Insufficient data to deskew
-        return im
-
-    # Average the angles to a degree offset
-    angle_deg = np.rad2deg(np.median(angles))
-
-    # If this is landscape image, rotate the entire canvas appropriately
-    if landscape:
-        if angle_deg < 0:
-            im = cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
-            angle_deg += 90
-        elif angle_deg > 0:
-            im = cv2.rotate(im, cv2.ROTATE_90_COUNTERCLOCKWISE)
-            angle_deg -= 90
-
-    # Rotate the image by the residual offset
-    M = cv2.getRotationMatrix2D((width / 2, height / 2), angle_deg, 1)
-    im = cv2.warpAffine(im, M, (width, height),
-                        borderMode=cv2.BORDER_REPLICATE)
-
-    # show_images([im])
-    return im
-
-
 def transformation(image):
     image = image.copy()
     originalImage = np.copy(image)
@@ -224,14 +117,13 @@ def transformation(image):
 
     if not should_rotate_edge_image(lines_edges):
         print('Should not process')
-        return originalImage
+        return originalImage    # show_images([image, threshold, dilate, edges], [
+    #             "Original", "Threshold", "Dilated", "Edges"])
 
     dilate = cv2.dilate(threshold, np.ones(
         (9, 9), np.uint8), iterations=8)
 
     edges = cv2.Canny(dilate, 50, 150, apertureSize=7)
-    # show_images([image, threshold, dilate, edges], [
-    #             "Original", "Threshold", "Dilated", "Edges"])
 
     ## FIND CONTOUR ##
     contours, hierarchy = cv2.findContours(
@@ -269,9 +161,6 @@ def transformation(image):
     box = np.int0(box)
     img_with_rect = cv2.drawContours(rect_img, [box], 0, (0, 0, 255), 2)
 
-    # show_images([image, img_with_contours, contouredImage, img_with_rect], [
-    #             "Image", "Contour", "Simplified Contour", "Min Area Rect"])
-
     dst = image
     dst = four_point_transform(image, box)
 
@@ -300,7 +189,7 @@ def final_image(rotated):
 def fix_rotation(image):
     edges = cv2.Canny(image, 50, 150, apertureSize=7)
     h, theta, d = hough_line(edges)
-    # peaks = cv2.hough_line_peaks(h, theta, d)
+
     max_index = np.unravel_index(h.argmax(), h.shape)
     degreePeak = np.rad2deg(theta[max_index[1]])
 
@@ -308,8 +197,10 @@ def fix_rotation(image):
     if(rotationAngle < 45):
         return image
 
-    rotatedImg = rotate(image, rotationAngle, cval=1, resize=True)
-    # show_images([rotatedImg])
+    rotatedImg = rotate(image, rotationAngle, cval=1,
+                        resize=True)
+    rotatedImg = (rotatedImg * 255).astype(np.uint8)
+
     return rotatedImg
 
 
@@ -330,8 +221,6 @@ def should_rotate(image):
 
 
 def fix_orientation(image):
-    # 1. Pass the image through the transformation function to crop out the biggest contour
-    # 2. Brighten & Sharpen the image to get a final cleaned image
     cleaned_image = image
     blurred_threshold = transformation(image)
     cleaned_image = final_image(blurred_threshold)
@@ -344,6 +233,7 @@ def read_and_preprocess_image(path):
     useAugmented = should_rotate(image)
     if useAugmented:
         image = fix_orientation(image)
+        image = ~image
     else:
         if len(image.shape) == 3:
             if image.shape[2] > 3:
